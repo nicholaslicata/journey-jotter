b73b6e8949a11b14ffcb50c002436cda389f7eda
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-02 20:04:35 -0400

    firebase config initial upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAB6oV9xyiH_pO4TldI5tZJ0zncShlh-KU",
+  authDomain: "journey-jotter.firebaseapp.com",
+  projectId: "journey-jotter",
+  storageBucket: "journey-jotter.appspot.com",
+  messagingSenderId: "218314577295",
+  appId: "1:218314577295:web:7f072a3cc8e7ff1fc9e7fa",
+  measurementId: "G-1RV76GE46W"
+};
+
+const app = initializeApp(firebaseConfig);
