98f3e6a9667a69a5f98fff93b7b733794d0e9820
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-07 23:35:42 -0400

    setup layout for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <main className='login-page'>
+            <form className='login-container'>
+                <h1 className='login-title'>Journey Jotter</h1>
+                <div className='login-btn-container'>
+                    <button type='button' className='select-register-btn'>Register</button>
+                    <button type='button' className='select-login-btn'>Log In</button>
+                </div>
+                <div className='email-input-container'>
+                    <label htmlFor='login-email' className='login-label'>Email</label>
+                    <input type='email' name='login-email' className='login-email-input' placeholder='Type your email' />
+                </div>
+                <div className='password-input-container'>
+                    <label htmlFor='login-password' className='login-label'>Password</label>
+                    <input type='password' name='login-password' className='login-password-input' placeholder='Type your password' />
+                </div>
+                <button className='user-login-btn'>Log In</button>
+                <button className='guest-login-btn'>Continue As Guest</button>
+            </form>
+        </main>
     )
 }
 
