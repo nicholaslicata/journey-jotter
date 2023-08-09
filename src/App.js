8aa036fb80a5b57105f6de96733eb0e3febd7b7b
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-08 23:09:56 -0400

    updated state for conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
+
+  const [page, setPage] = useState('login');
+
   return (
     <div className="App">
-      <Login />
+      {page === 'login' && <Login page={page} setPage={setPage} />}
+      {page === 'register' && <Register page={page} setPage={setPage} />}
     </div>
   );
 }
