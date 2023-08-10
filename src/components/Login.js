8e2f0d345559f15476fdd2d8225faae5806844dd
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-09 20:21:36 -0400

    updated select btn class

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ function Login({ page, setPage }) {
                 <h1 className='login-title'>Journey Jotter</h1>
                 <div className='login-btn-container'>
                     <button onClick={() => setPage('register')} type='button' className='select-register-btn'>Register</button>
-                    <button type='button' className='select-login-btn'>Log In</button>
+                    <button type='button' className='select-login-active'>Log In</button>
                 </div>
                 <div className='email-input-container'>
                     <label htmlFor='login-email' className='login-label'>Email</label>
@@ -16,7 +16,7 @@ function Login({ page, setPage }) {
                     <label htmlFor='login-password' className='login-label'>Password</label>
                     <input type='password' name='login-password' className='login-password-input' placeholder='Type your password' />
                 </div>
-                <button className='user-login-btn'>Log In</button>
+                <button className='user-login-btn'>Log In To Account</button>
                 <button className='guest-login-btn'>Continue As Guest</button>
             </form>
         </main>
