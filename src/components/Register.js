faba28263bfd2a063daf54696327729f5ece02d1
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-11 21:37:49 -0400

    setup user registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-function Register({ page, setPage}) {
+function Register({ page, setPage }) {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function registerUser(e) {
+        e.preventDefault();
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            const user = response.user;
+            setPage('home');
+            console.log(user);
+        }
+        catch(err){
+            console.log(err.message);
+        }
+    }
+
     return (
         <main className='register-page'>
-            <form className='register-container'>
+            <form onSubmit={registerUser} className='register-container'>
                 <h1 className='login-title'>Journey Jotter</h1>
                 <div className='register-btn-container'>
                     <button type='button' className='select-register-active'>Register</button>
@@ -10,13 +30,13 @@ function Register({ page, setPage}) {
                 </div>
                 <div className='email-input-container'>
                     <label htmlFor='login-email' className='login-label'>Email</label>
-                    <input type='email' name='login-email' className='login-email-input' placeholder='Type your email' />
+                    <input onChange={(e) => setEmail(e.target.value)} type='email' name='login-email' className='login-email-input' placeholder='Type your email' autoComplete='off' />
                 </div>
                 <div className='password-input-container'>
                     <label htmlFor='login-password' className='login-label'>Password</label>
-                    <input type='password' name='login-password' className='login-password-input' placeholder='Type your password' />
+                    <input onChange={(e) => setPassword(e.target.value)} type='password' name='login-password' className='login-password-input' placeholder='Type your password' autoComplete='off' />
                 </div>
-                <button className='user-register-btn'>Register</button>
+                <button type='submit' onClick={registerUser} className='user-register-btn'>Register</button>
             </form>
         </main>
     )
