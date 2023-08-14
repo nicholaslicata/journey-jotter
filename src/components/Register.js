240eba6a115f194b3178c0724d7caaee1405c7ba
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-13 21:39:21 -0400

    setup register error handling

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { auth } from '../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-function Register({ page, setPage }) {
+function Register({ page, setPage, error, setError, errorMessage, setErrorMessage }) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -10,13 +10,28 @@ function Register({ page, setPage }) {
     async function registerUser(e) {
         e.preventDefault();
         try {
+            if (email && password) {
             const response = await createUserWithEmailAndPassword(auth, email, password);
             const user = response.user;
             setPage('home');
+            setError(false);
             console.log(user);
+            } else if (!email || !password) {
+            setError(true);
+            setErrorMessage('Please fill out all text fields.');
+            }
         }
         catch(err){
-            console.log(err.message);
+            if (err.code === 'auth/weak-password') {
+                setError(true);
+                setErrorMessage('Password must be at least 6 characters.');
+            } else if (err.code === 'auth/invalid-email') {
+                setError(true);
+                setErrorMessage('Please provide a valid email address.');
+            } else if (err.code === 'auth/email-already-in-use') {
+                setError(true);
+                setErrorMessage('Email address is already in use.')
+            }
         }
     }
 
@@ -24,9 +39,10 @@ function Register({ page, setPage }) {
         <main className='register-page'>
             <form onSubmit={registerUser} className='register-container'>
                 <h1 className='login-title'>Journey Jotter</h1>
+                {error && errorMessage ? <span className='register-error'>{errorMessage}</span> : null}
                 <div className='register-btn-container'>
                     <button type='button' className='select-register-active'>Register</button>
-                    <button onClick={() => setPage('login')} type='button' className='select-login-btn'>Log In</button>
+                    <button onClick={() => {setPage('login'); setError(false);}} type='button' className='select-login-btn'>Log In</button>
                 </div>
                 <div className='email-input-container'>
                     <label htmlFor='login-email' className='login-label'>Email</label>
