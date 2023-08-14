2f4deffcabe225ed2f20887da562fb3e580c57f2
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-13 21:37:49 -0400

    added state for error

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Home from './components/Home';
 function App() {
 
   const [page, setPage] = useState('login');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <div className="App">
-      {page === 'login' && <Login page={page} setPage={setPage} />}
-      {page === 'register' && <Register page={page} setPage={setPage} />}
+      {page === 'login' && <Login page={page} setPage={setPage} error={error} setError={setError} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}
+      {page === 'register' && <Register page={page} setPage={setPage} error={error} setError={setError} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}
       {page === 'home' && <Home />}
     </div>
   );
