e36175f59002cb1f33a423c4688bd834c58db230
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-13 21:38:55 -0400

    setup login error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { auth } from '../firebase'
 import { signInAnonymously, signInWithEmailAndPassword } from 'firebase/auth';
 
-function Login({ page, setPage }) {
+function Login({ page, setPage, error, setError, errorMessage, setErrorMessage }) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -10,11 +10,25 @@ function Login({ page, setPage }) {
     async function logInUser(e) {
         e.preventDefault();
         try {
-            await signInWithEmailAndPassword(auth, email, password);
+            if (email && password) {
+            const response = await signInWithEmailAndPassword(auth, email, password);
+            const user = response.user;
             setPage('home');
+            setError(false);
+            console.log(user);
+            } else if (!email || !password) {
+                setError(true);
+                setErrorMessage('Please fill out all text fields.');
+            }
         }
         catch(err) {
-            console.log(err.message);
+           if (err.code === 'auth/wrong-password') {
+            setError(true);
+            setErrorMessage('Password is incorrect.')
+           } else if (err.code === 'auth/user-not-found') {
+            setError(true);
+            setErrorMessage('Email address not found.')
+           }
         }
     }
 
@@ -31,19 +45,20 @@ function Login({ page, setPage }) {
 
     return (
         <main className='login-page'>
-            <form className='login-container'>
+            <form className='login-container' noValidate>
                 <h1 className='login-title'>Journey Jotter</h1>
+                {error && errorMessage ? <span className='login-error'>{errorMessage}</span> : null}
                 <div className='login-btn-container'>
-                    <button onClick={() => setPage('register')} type='button' className='select-register-btn'>Register</button>
+                    <button onClick={() => {setPage('register'); setError(false);}} type='button' className='select-register-btn'>Register</button>
                     <button type='button' className='select-login-active'>Log In</button>
                 </div>
                 <div className='email-input-container'>
                     <label htmlFor='login-email' className='login-label'>Email</label>
-                    <input onChange={(e) => setEmail(e.target.value)} type='email' name='login-email' className='login-email-input' placeholder='Type your email' autoComplete="off" />
+                    <input onChange={(e) => setEmail(e.target.value)} type='email' name='login-email' className='login-email-input' placeholder='Type your email' autoComplete="off" minLength='6' required />
                 </div>
                 <div className='password-input-container'>
                     <label htmlFor='login-password' className='login-label'>Password</label>
-                    <input onChange={(e) => setPassword(e.target.value)} type='password' name='login-password' className='login-password-input' placeholder='Type your password' autoComplete="off" />
+                    <input onChange={(e) => setPassword(e.target.value)} type='password' name='login-password' className='login-password-input' placeholder='Type your password' autoComplete="off" minLength='6' required />
                 </div>
                 <button type='submit' onClick={logInUser} className='user-login-btn'>Log In To Account</button>
                 <button type='submit' onClick={logInGuest} className='guest-login-btn'>Continue As Guest</button>
