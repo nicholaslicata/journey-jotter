dd17e077cd940faeacee231d1098e00341a229a6
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-19 19:22:41 -0400

    added firestore to project

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAB6oV9xyiH_pO4TldI5tZJ0zncShlh-KU",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
