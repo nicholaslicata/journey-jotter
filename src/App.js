87e270096d5abe73fd3c20c11521b0f47cfb49e9
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-23 20:55:08 -0400

    added state for list

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ function App() {
   const [page, setPage] = useState('login');
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [list, setList] = useState([]);
 
   return (
     <div className="App">
       {page === 'login' && <Login page={page} setPage={setPage} error={error} setError={setError} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}
       {page === 'register' && <Register page={page} setPage={setPage} error={error} setError={setError} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}
-      {page === 'home' && <Home setPage={setPage} />}
+      {page === 'home' && <Home setPage={setPage} list={list} setList={setList} />}
     </div>
   );
 }
