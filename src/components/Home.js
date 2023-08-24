585f11c95a6c2a8fe798af105082374e0c7ab7f2
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-23 20:56:09 -0400

    setup functionality to get data from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
+import { db } from '../firebase';
+import { getDocs, collection } from 'firebase/firestore';
 import Nav from '../components/Nav';
+import List from '../components/List';
 
-function Home({ setPage }) {
+function Home({ setPage, list, setList }) {
+
+    const listRef = collection(db, 'list');
+
+    useEffect(() => {
+        getList();
+    }, [])
+
+    async function getList() {
+        const response = await getDocs(listRef);
+        const filteredResponse = response.docs.map((doc) => ({...doc.data(), id: doc.id,}))
+        setList(filteredResponse);
+        console.log(filteredResponse);
+    }
+
     return (
         <main className='home-page'>
             <Nav setPage={setPage} />
+            <List list={list} />
         </main>
     )
 }
