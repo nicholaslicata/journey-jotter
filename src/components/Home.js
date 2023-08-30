a337fec2450186b815f9570cf51be68f8817bb39
Nicholas Licata <nicholas.licata730@gmail.com>
2023-08-29 20:28:12 -0400

    added state for form inputs

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,17 @@ import { db } from '../firebase';
 import { getDocs, collection } from 'firebase/firestore';
 import Nav from '../components/Nav';
 import List from '../components/List';
+import AddItemForm from '../components/AddItemForm';
+import ItemDetails from '../components/ItemDetails';
 
-function Home({ setPage, list, setList }) {
+function Home({ setPage, subPage, setSubPage, list, setList }) {
 
     const listRef = collection(db, 'list');
+
+    const [newTitle, setNewTitle] = useState('');
+    const [newLocation, setNewLocation] = useState('');
+    const [newDate, setNewDate]  = useState(0);
+    const [newDescription, setNewDescription] = useState('');
 
     useEffect(() => {
         getList();
@@ -16,13 +23,17 @@ function Home({ setPage, list, setList }) {
         const response = await getDocs(listRef);
         const filteredResponse = response.docs.map((doc) => ({...doc.data(), id: doc.id,}))
         setList(filteredResponse);
-        console.log(filteredResponse);
     }
 
     return (
         <main className='home-page'>
             <Nav setPage={setPage} />
-            <List list={list} />
+            <div className='add-btn-container'>
+                <button onClick={() => setSubPage('addItemForm')} className='add-item-btn'>Add Item</button>
+            </div>
+            {subPage === 'list' && <List list={list} setSubPage={setSubPage} />}
+            {subPage === 'addItemForm' && <AddItemForm listRef={listRef} setSubPage={setSubPage} getList={getList} />}
+            {subPage === 'itemDetails' && <ItemDetails />}
         </main>
     )
 }
