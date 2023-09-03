7e9d03c802fe5add1ac19657d5994e4d4a638d58
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-02 22:23:40 -0400

    added state for status and completed date

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,10 @@ function Home({ setPage, subPage, setSubPage, list, setList }) {
 
     const [newTitle, setNewTitle] = useState('');
     const [newLocation, setNewLocation] = useState('');
-    const [newDate, setNewDate]  = useState(0);
+    const [newTargetDate, setNewTargetDate]  = useState('');
     const [newDescription, setNewDescription] = useState('');
+    const [newStatus, setNewStatus] = useState('');
+    const [newCompletedDate, setNewCompletedDate] = useState('');
     const [currentItem, setCurrentItem] = useState();
 
     useEffect(() => {
@@ -30,11 +32,11 @@ function Home({ setPage, subPage, setSubPage, list, setList }) {
 
     return (
         <main className='home-page'>
-            <Nav setPage={setPage} />
+            <Nav setPage={setPage} setSubPage={setSubPage} />
             {subPage === 'list' && <AddItemBtn setSubPage={setSubPage} />}
             {subPage === 'addItemForm' || subPage === 'itemDetails' ? <BackBtn setSubPage={setSubPage} /> : null}
             {subPage === 'list' && <List list={list} setSubPage={setSubPage} setCurrentItem={setCurrentItem} />}
-            {subPage === 'addItemForm' && <AddItemForm listRef={listRef} setSubPage={setSubPage} getList={getList} newTitle={newTitle} setNewTitle={setNewTitle} newLocation={newLocation} setNewLocation={setNewLocation} newDate={newDate} setNewDate={setNewDate} newDescription={newDescription} setNewDescription={setNewDescription}/>}
+            {subPage === 'addItemForm' && <AddItemForm listRef={listRef} setSubPage={setSubPage} getList={getList} newTitle={newTitle} setNewTitle={setNewTitle} newLocation={newLocation} setNewLocation={setNewLocation} newTargetDate={newTargetDate} setNewTargetDate={setNewTargetDate} newDescription={newDescription} setNewDescription={setNewDescription} newStatus={newStatus} setNewStatus={setNewStatus} newCompletedDate={newCompletedDate} setNewCompletedDate={setNewCompletedDate} />}
             {subPage === 'itemDetails' && <ItemDetails currentItem={currentItem} />}
         </main>
     )
