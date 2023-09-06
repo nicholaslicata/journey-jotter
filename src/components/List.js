b3c1d9c14c0a78899fc160f07c8935f8e5e46f14
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-05 20:58:11 -0400

    added conditional rendering for empty list

--- src/components/List.js
+++ src/components/List.js
@@ -4,9 +4,12 @@ function List({ list, setSubPage, setCurrentItem }) {
 
     return (
         <section className='list-container'>
-             {list.map((item) => (
+             {list.length > 0 ? list.map((item) => (
                     <ListItem item={item} setSubPage={setSubPage} setCurrentItem={setCurrentItem} />
-            ))} 
+            )) : (<div className='empty-list-container'>
+                <p className='empty-list-text1'>You have nothing to show</p>
+                <p className='empty-list-text2'>Click the add button to jot a new item to your journey</p>
+            </div>)} 
         </section>
     )
 }
