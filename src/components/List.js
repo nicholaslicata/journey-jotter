2152e0c201a8857ab495ef29f6fc6ee6f42a6fe4
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-18 21:50:45 -0400

    updated props

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,11 @@
 import ListItem from '../components/ListItem';
 
-function List({ list, setSubPage, setCurrentItem }) {
+function List({ list, setSubPage, setCurrentItem, currentItem }) {
 
     return (
         <section className='list-container'>
              {list.length > 0 ? list.map((item) => (
-                    <ListItem item={item} setSubPage={setSubPage} setCurrentItem={setCurrentItem} />
+                    <ListItem item={item} setSubPage={setSubPage} setCurrentItem={setCurrentItem} currentItem={currentItem} />
             )) : (<div className='empty-list-container'>
                 <p className='empty-list-text1'>You have nothing to show</p>
                 <p className='empty-list-text2'>Click the add button to jot a new item to your journey</p>
