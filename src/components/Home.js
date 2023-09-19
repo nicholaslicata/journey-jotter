563bbe471c01e614eca14835f14a60c4fe535c39
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-18 21:50:00 -0400

    updated props for edit item form

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,17 +7,24 @@ import AddItemForm from '../components/AddItemForm';
 import ItemDetails from '../components/ItemDetails';
 import AddItemBtn from '../components/AddItemBtn';
 import BackBtn from '../components/BackBtn';
+import EditItemForm from '../components/EditItemForm';
 
 function Home({ setPage, subPage, setSubPage, list, setList, userId }) {
 
     const listRef = collection(db, 'list');
 
     const [newTitle, setNewTitle] = useState('');
+    const [editedTitle, setEditedTitle] = useState('');
     const [newLocation, setNewLocation] = useState('');
+    const [editedLocation, setEditedLocation] = useState('');
     const [newTargetDate, setNewTargetDate]  = useState('');
+    const [editedTargetDate, setEditedTargetDate] = useState('');
     const [newDescription, setNewDescription] = useState('');
+    const [editedDescription, setEditedDescription] = useState();
     const [newStatus, setNewStatus] = useState('');
+    const [editedStatus, setEditedStatus] = useState('')
     const [newCompletedDate, setNewCompletedDate] = useState('');
+    const [editedCompletedDate, setEditedCompletedDate] = useState('')
     const [currentItem, setCurrentItem] = useState();
 
     useEffect(() => {
@@ -27,17 +34,14 @@ function Home({ setPage, subPage, setSubPage, list, setList, userId }) {
     async function getList() {
         try {
         const listQuery = query(listRef, where('userId', '==', userId));
-        
         onSnapshot(listQuery, (snapshot) => {
             let docs = [];
-            
             snapshot.forEach((doc) => {
                 const data = doc.data();
                 const id = doc.id;
 
                 docs.push({...data, id});
             })
-
             setList(docs);
         })
         }
@@ -50,10 +54,11 @@ function Home({ setPage, subPage, setSubPage, list, setList, userId }) {
         <main className='home-page'>
             <Nav setPage={setPage} setSubPage={setSubPage} />
             {subPage === 'list' && <AddItemBtn setSubPage={setSubPage} />}
-            {subPage === 'addItemForm' || subPage === 'itemDetails' ? <BackBtn setSubPage={setSubPage} /> : null}
-            {subPage === 'list' && <List list={list} setSubPage={setSubPage} setCurrentItem={setCurrentItem} />}
+            {subPage === 'addItemForm' || subPage === 'itemDetails' || subPage === 'editItemForm' ? <BackBtn subPage={subPage} setSubPage={setSubPage} /> : null}
+            {subPage === 'list' && <List list={list} setSubPage={setSubPage} setCurrentItem={setCurrentItem} currentItem={currentItem} />}
             {subPage === 'addItemForm' && <AddItemForm listRef={listRef} setSubPage={setSubPage} newTitle={newTitle} setNewTitle={setNewTitle} newLocation={newLocation} setNewLocation={setNewLocation} newTargetDate={newTargetDate} setNewTargetDate={setNewTargetDate} newDescription={newDescription} setNewDescription={setNewDescription} newStatus={newStatus} setNewStatus={setNewStatus} newCompletedDate={newCompletedDate} setNewCompletedDate={setNewCompletedDate} userId={userId} />}
             {subPage === 'itemDetails' && <ItemDetails currentItem={currentItem} setSubPage={setSubPage} />}
+            {subPage === 'editItemForm' && <EditItemForm currentItem={currentItem} editedTitle={editedTitle} setEditedTitle={setEditedTitle} editedLocation={editedLocation} setEditedLocation={setEditedLocation} editedTargetDate={editedTargetDate} setEditedTargetDate={setEditedTargetDate} editedDescription={editedDescription} setEditedDescription={setEditedDescription} editedStatus={editedStatus} setEditedStatus={setEditedStatus} editedCompletedDate={editedCompletedDate} setEditedCompletedDate={setEditedCompletedDate} setSubPage={setSubPage} />}
         </main>
     )
 }
