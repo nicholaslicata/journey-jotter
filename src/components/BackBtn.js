389dcab09dd1e680bb59a362836f15edbb1d7242
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-20 21:17:21 -0400

    clear form state when using back button

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -1,8 +1,8 @@
 
-function BackBtn({ subPage, setSubPage }) {
+function BackBtn({ subPage, setSubPage, setNewTitle, setNewLocation, setNewTargetDate, setNewDescription, setNewStatus, setEditedTitle, setEditedLocation, setEditedTargetDate, setEditedDescription, setEditedStatus }) {
     return (
         <div className='back-btn-container'>
-            <button onClick={() => {subPage === 'editItemForm' ? setSubPage('itemDetails') : setSubPage('list')}} className='back-btn'>Go Back</button>
+            <button onClick={() => {subPage === 'editItemForm' ? setSubPage('itemDetails') : setSubPage('list'); setNewTitle(''); setNewLocation(''); setNewTargetDate(''); setNewDescription(''); setNewStatus(''); setEditedTitle(''); setEditedLocation(''); setEditedTargetDate(''); setEditedDescription(''); setEditedStatus('');}} className='back-btn'>Go Back</button>
         </div>
     )
 }
