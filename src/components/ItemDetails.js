2b72bfba2c5394db05555d90b1cba497b764f9c3
Nicholas Licata <nicholas.licata730@gmail.com>
2023-09-26 21:35:39 -0400

    setup page layout for item details

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,12 +1,38 @@
+import DeleteItemBtn from '../components/DeleteItemBtn';
+import EditItemBtn from '../components/EditItemBtn';
 
-function ItemDetails({ currentItem }) {
+function ItemDetails({ currentItem, setSubPage }) {
     return (
-        <div>
-        <p>{currentItem.title}</p>
-        <p>{currentItem.location}</p>
-        <p>{currentItem.date}</p>
-        <p>{currentItem.description}</p>
-        </div>
+        <section className='item-details-page-container'>
+          <div className='item-details-container'>
+            <div className='item-details-btn-container'>
+              <DeleteItemBtn currentItem={currentItem} setSubPage={setSubPage} />
+              <EditItemBtn setSubPage={setSubPage} />
+            </div>
+            <div className='item-details-text-container'>
+                <ul className='item-detail'>
+                    <li className='item-detail-heading'>Goal</li>
+                    <li className='item-detail-text'>{currentItem.title}</li>
+                </ul>
+                <ul className='item-detail'>
+                    <li className='item-detail-heading'>Location</li>
+                    <li className='item-detail-text'>{currentItem.location}</li>
+                </ul>
+                <ul className='item-detail'>
+                    <li className='item-detail-heading'>Target Date</li>
+                    <li className='item-detail-text'>{currentItem.targetDate}</li>
+                </ul>
+                <ul className='item-detail'>
+                    <li className='item-detail-heading'>Description</li>
+                    <li className='item-detail-text'>{currentItem.description}</li>
+                </ul>
+                <ul className='item-detail'>
+                    <li className='item-detail-heading'>Status</li>
+                    <li className='item-detail-text'>{currentItem.status}</li>
+                </ul>
+            </div>
+          </div>
+        </section>
     )
 }
 
